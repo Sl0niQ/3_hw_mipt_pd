72c954afced5edc8cbb64e2b1a13125b817b6a97
Sl0niQ <31eriflleh@gmail.com>
2025-07-20 01:11:28 +0700

    task 2 added to main; activate.bat added to .gitignore

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+#------------------------1------------------------
 """
 	реализуйте:
 	Декоратор @even, который будет заменять функцию на идентичную, но каждое нечётное по 
@@ -17,9 +18,29 @@ def even(func):
 	return wrapper
 
 @even # == print_call_counter = even(print_call_counter)
-def print_call_number(i):
-	print('i = ', i)
+def print_call_number(x):
+	print("hello", x)
 
 for i in range(1, 5):
 	print_call_number(i)
 
+#------------------------2------------------------
+"""
+	Декоратор @clip, который пробрасывает в функцию все позиционные аргументы, при этом не 
+	пробрасывает ключевые (например @clip от print(1, 2, 3, sep="_") напечатает "1 2 3", не 
+	применив sep)
+"""
+
+def clip(func):
+	def wrapper(*args, **kwargs):
+		return func(*args)
+	return wrapper
+
+@clip
+def print_clip(x, y, z = 0, s = "~"):
+    print(x, y, z, sep = s)
+
+print_clip(1, 2, z = 3, s = "_")       # 1~2~0
+print(1, 2, 3, sep = "_")              # 1_2_3
+
+#------------------------3------------------------
